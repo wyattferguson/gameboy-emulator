c0a6314628d89fef6f12f056a57e7c7c44a84140
Wyatt Ferguson <wyattf@gmail.com>
2025-04-08 13:16:06 -0400

    dpad bit flip

--- gbemu/joypad.py
+++ gbemu/joypad.py
@@ -14,7 +14,7 @@ class Joypad:
         """Set pressed buttons."""
         # Reset dpad / button flags
         if self.dpad_pressed != dpad:
-            self.ram[JOYPAD_MEM] = 0xFF
+            self.ram[JOYPAD_MEM] = 0xFF ^ (0b0001_0000 if dpad else 0b0010_0000)
             self.dpad_pressed = dpad
         # Flip the bits of the pressed buttons
         self.ram[JOYPAD_MEM] ^= bits
